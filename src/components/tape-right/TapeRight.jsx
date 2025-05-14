451497da15c669e1284cf429faa29d344fdf8480
AllanMicuanski <allanmicuanski@gmail.com>
2025-05-13 22:56:28 -0300

    added tape right

---
+++ src/components/tape-right/TapeRight.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TapeRight.css';  
+
+const TapeRight = () => {
+  return (
+    <div className="tape-right">
+      <img
+        src="src/assets/TapeRight.svg"  
+        alt="Tape Right"
+      />
+    </div>
+  );
+};
+
+export default TapeRight;
