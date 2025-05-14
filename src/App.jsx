510eeb703291ae0d8cbc2b828df044d6fc93899b
AllanMicuanski <allanmicuanski@gmail.com>
2025-05-13 22:56:28 -0300

    added tape right

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+import './index.css'
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
 import About from './sections/about/About';
 import Services from './sections/services/Services';
 import Contact from './sections/contact/Contact';
 import Footer from './components/footer/Footer';
-import './index.css'
+import TapeRight from './components/tape-right/TapeRight'
 
 function App() {
   return (
     <div className="app-background">
       <Header />
-      <main className="pt-20">
+      <main >
         <Hero />
+         <TapeRight />
       </main>
       <Footer />
     </div>
