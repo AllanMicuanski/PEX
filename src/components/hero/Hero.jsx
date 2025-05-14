9b4d51ec6b46525d13554ed0a8959c84909327f9
AllanMicuanski <allanmicuanski@gmail.com>
2025-05-13 22:09:57 -0300

    added css vanila

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <section className="hero-section" id="home">
+      <div className="hero-content">
+        <h2 className="hero-title">Bem-vindo à MinhaEmpresa</h2>
+        <p className="hero-subtitle">Soluções inteligentes para o seu negócio.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
