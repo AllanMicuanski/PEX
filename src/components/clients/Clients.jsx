1acd7d7d17569878068910499d644b1f4ddc90f0
AllanMicuanski <allanmicuanski@gmail.com>
2025-05-14 22:16:44 -0300

    added clients page

---
+++ src/components/clients/Clients.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Clients.css';
+
+const Clients = () => {
+  return (
+    <section id="Clients" className="Clients-section">
+      <h2 className="Clients-title">Clientes</h2>
+    </section>
+  );
+};
+
+export default Clients;
