345e827e869d3ea8df6fb113a0057371a651ea88
AllanMicuanski <allanmicuanski@gmail.com>
2025-05-14 22:07:47 -0300

    added how-work page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import './index.css'
+import './index.css';
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
 import About from './components/about/About';
 import Footer from './components/footer/Footer';
-import TapeRight from './components/tape-right/TapeRight'
+import TapeRight from './components/tape-right/TapeRight';
 import TapeLeft from './components/tape-left/TapeLeft';
+import HowWork from './components/howWork/HowWork';
 
 function App() {
   return (
     <div className="app-background">
       <Header />
-      <main >
+      <main>
         <Hero />
-      <TapeRight />
-      <About/>
-      <TapeLeft />
-
+        <TapeRight />
+        <About />
+        <TapeLeft />
+        <HowWork />
       </main>
       <Footer />
     </div>
