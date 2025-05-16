054c8e6dfebbd8c87735d514e4862ec737fc0c19
AllanMicuanski <allanmicuanski@gmail.com>
2025-05-16 20:49:29 -0300

    feat logo header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from "react";
 import "./Header.css";
 
 function Header() {
+  const [showAudaces, setShowAudaces] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowAudaces(true);
+    }, 2000); // tempo para trocar a logo
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <header>
       <div className="container">
-        {/* Logo */}
         <div className="logo-container">
-          <img
-            src="/src/assets/logoHeaderDark.svg"
-            alt="Logo"
-          />
+          {!showAudaces ? (
+            <img
+              className="logo logo-white fade-in"
+              src="/src/assets/logoHeaderWhite.svg"
+              alt="Logo Sizebay White"
+            />
+          ) : (
+            <>
+              <img
+                className="logo logo-dark fade-in"
+                src="/src/assets/logoHeaderDark.svg"
+                alt="Logo Sizebay Dark"
+              />
+              <span className="logo-span fade-in">by:</span>
+              <img
+                className="logo logo-audaces fade-in"
+                src="/src/assets/logo-audaces.png"
+                alt="Logo Audaces"
+              />
+            </>
+          )}
         </div>
 
         {/* Navegação */}
